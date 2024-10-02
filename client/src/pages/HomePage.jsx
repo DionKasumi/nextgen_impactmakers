016d6074c6adae82d2eee97c3a240ec01ca22acf
DingoBruh <dingobruh1@gmail.com>
2024-10-02 10:46:42 +0200

    Updated SignUp Form to print out the input data to the console

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react';
 import SectionWrapper from '../hoc/SectionWrapper';
 import { IoMdArrowDropdown } from 'react-icons/io';
--- client/src/pages/auth/SignupPage.jsx
+++ client/src/pages/auth/SignupPage.jsx
@@ -1,89 +1,209 @@
+import { useState } from 'react';
+
+const ParticipantForm = ({ participantData, handleChange }) => {
+    return (
+        <div className="flex flex-col w-full">
+            <input
+                type="text"
+                name="name"
+                id="name"
+                value={participantData.name}
+                onChange={handleChange}
+                placeholder="Name"
+                required
+                className="w-full h-auto p-4 md:p-5 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+            <input
+                type="email"
+                name="email"
+                id="email"
+                value={participantData.email}
+                onChange={handleChange}
+                placeholder="email@live.com"
+                required
+                className="w-full h-auto p-4 md:p-5 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+            <input
+                type="text"
+                name="phone_number"
+                id="phone_number"
+                value={participantData.phone_number}
+                onChange={handleChange}
+                placeholder="Phone number"
+                required
+                className="w-full h-auto p-4 md:p-5 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+            <input
+                type="password"
+                name="password"
+                id="password"
+                value={participantData.password}
+                onChange={handleChange}
+                placeholder="Password"
+                required
+                className="w-full h-auto p-4 md:p-5 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+        </div>
+    );
+};
+
+const OrganizationForm = ({ orgData, handleChange }) => {
+    return (
+        <div className="flex flex-col w-full">
+            <input
+                type="text"
+                name="name_of_org"
+                id="name_of_org"
+                value={orgData.name_of_org}
+                onChange={handleChange}
+                placeholder="Name Of Organization"
+                required
+                className="w-full h-auto p-2 md:p-3 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+            <input
+                type="email"
+                name="email_of_org"
+                id="email_of_org"
+                value={orgData.email_of_org}
+                onChange={handleChange}
+                placeholder="email@live.com"
+                required
+                className="w-full h-auto p-2 md:p-3 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+            <input
+                type="text"
+                name="phone_number_of_org"
+                id="phone_number_of_org"
+                value={orgData.phone_number_of_org}
+                onChange={handleChange}
+                placeholder="Phone number"
+                required
+                className="w-full h-auto p-2 md:p-3 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+            <input
+                type="password"
+                name="password_of_org"
+                id="password_of_org"
+                value={orgData.password_of_org}
+                onChange={handleChange}
+                placeholder="Password"
+                required
+                className="w-full h-auto p-2 md:p-3 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+            <input
+                type="text"
+                name="url_of_org"
+                id="url_of_org"
+                value={orgData.url_of_org}
+                onChange={handleChange}
+                placeholder="Url"
+                required
+                className="w-full h-auto p-2 md:p-3 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            />
+            <textarea
+                name="description_of_org"
+                id="description_of_org"
+                value={orgData.description_of_org}
+                onChange={handleChange}
+                rows={3}
+                className="w-full h-auto p-2 md:p-3 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2 resize-none"
+                required
+                placeholder="A brief description of your organization..."
+            ></textarea>
+        </div>
+    );
+};
+
 const MainForm = () => {
+    const [isOrg, setOrg] = useState(false);
+    const [participantData, setParticipantData] = useState({
+        name: '',
+        email: '',
+        phone_number: '',
+        password: '',
+    });
+    const [orgData, setOrgData] = useState({
+        name_of_org: '',
+        email_of_org: '',
+        phone_number_of_org: '',
+        password_of_org: '',
+        url_of_org: '',
+        description_of_org: '',
+    });
+
+    const handleParticipantChange = (e) => {
+        setParticipantData({
+            ...participantData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleOrgChange = (e) => {
+        setOrgData({
+            ...orgData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const onFormTypeChange = () => {
+        setOrg(!isOrg);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (isOrg) {
+            console.log('Organization Data:', orgData);
+        } else {
+            console.log('Participant Data:', participantData);
+        }
+    };
+
     return (
         <div className="w-11/12 sm:w-4/5 md:w-5/6 xl:w-5/6 h-[95%] flex justify-center items-center flex-col">
             <h2 className="text-xl md:text-2xl xl:text-3xl text-black font-bold mt-4">
                 Sign Up
             </h2>
-            <h2 className="text-sm md:text-base xl:text-lg text-black italic font-normal mb-4">
+            <h2 className="text-sm md:text-base xl:text-lg text-black italic font-normal mb-1">
                 Create An Account
             </h2>
             <h3 className="font-bold mb-4 text-base md:text-lg">Are you a</h3>
-            <div className="flex flex-row justify-center items-center w-full md:w-2/3 lg:w-1/2 mb-6">
-                <button className="bg-[#4F1ABE] border-none rounded-md focus:outline-none py-2 px-4 md:px-6 text-white text-sm">
+            <div className="flex flex-row justify-center items-center w-full md:w-2/3 lg:w-1/2 mb-4">
+                <button
+                    disabled={!isOrg}
+                    onClick={onFormTypeChange}
+                    type="button"
+                    className="bg-[#4F1ABE] border-none rounded-md focus:outline-none py-2 px-4 md:px-6 text-white text-sm disabled:bg-[#8d67e0] disabled:scale-95"
+                >
                     Participant
                 </button>
                 <p className="mx-2 text-sm md:text-base">or</p>
-                <button className="bg-[#4F1ABE] border-none rounded-md focus:outline-none py-2 px-4 md:px-6 text-white text-sm">
+                <button
+                    disabled={isOrg}
+                    onClick={onFormTypeChange}
+                    type="button"
+                    className="bg-[#4F1ABE] border-none rounded-md focus:outline-none py-2 px-4 md:px-6 text-white text-sm disabled:bg-[#8d67e0] disabled:scale-95"
+                >
                     Organization
                 </button>
             </div>
-            <div className="flex flex-col w-full">
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Name"
-                    required
-                    className="w-full h-auto p-4 md:p-5 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            {!isOrg ? (
+                <ParticipantForm
+                    participantData={participantData}
+                    handleChange={handleParticipantChange}
                 />
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="email@live.com"
-                    required
-                    className="w-full h-auto p-4 md:p-5 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
+            ) : (
+                <OrganizationForm
+                    orgData={orgData}
+                    handleChange={handleOrgChange}
                 />
-                <input
-                    type="text"
-                    name="phone_number"
-                    id="phone_number"
-                    placeholder="Phone number"
-                    required
-                    className="w-full h-auto p-4 md:p-5 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
-                />
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                    required
-                    className="w-full h-auto p-4 md:p-5 border-[#6153CC] border-[1px] focus:outline-none invalid:border-red-500 my-2"
-                />
-            </div>
+            )}
             <button
-                type="button"
+                type="submit"
+                onClick={onSubmit}
                 className="py-2 px-16 lg:px-24 bg-[#4F1ABE] text-white flex justify-center items-center rounded-md m-auto my-4 text-sm md:text-base"
             >
-                Continue
+                {isOrg ? 'Finish' : 'Continue'}
             </button>
-            <div className="w-full h-max flex flex-col justify-center items-center mb-4">
-                <p className="mb-4 text-sm md:text-base">
-                    or create an account with
-                </p>
-                <div className="flex flex-row space-x-10 justify-center">
-                    <img
-                        src="../assets/instagram_logo.svg"
-                        alt="instagram"
-                        className="w-6 h-8 hover:scale-105 transition-transform"
-                    />
-                    <img
-                        src="../assets/facebook_logo.svg"
-                        alt="facebook"
-                        className="w-6 h-8 hover:scale-105 transition-transform"
-                    />
-                    <img
-                        src="../assets/google_logo.svg"
-                        alt="google"
-                        className="w-6 h-8 hover:scale-105 transition-transform"
-                    />
-                    <img
-                        src="../assets/linkedin_logo.svg"
-                        alt="linkedin"
-                        className="w-6 h-8 hover:scale-105 transition-transform"
-                    />
-                </div>
-            </div>
         </div>
     );
 };
