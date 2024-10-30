017484c5e3da2f2eaf85ede5490e11fa24da682e
alenaab <alenaberisha16@gmail.com>
2024-10-30 11:09:40 +0100

    User Edit Profile page done

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -245,26 +245,33 @@ const NavBar = ({ theme }) => {
 
     return (
         <>
-            <nav
-                className="w-full h-20 flex justify-center items-center fixed top-0 left-0 z-50"
-                style={
-                    theme === 'primary'
-                        ? {
-                              backgroundImage: 'url(/assets/nav.png)', // Replace with your image path
-                              backgroundSize: 'cover',
-                              backgroundPosition: 'center',
-                              backgroundRepeat: 'no-repeat',
-                              color: 'white',
-                          }
-                        : { backgroundColor: '#ffffff', color: 'black' }
-                }
-            >
+           <nav
+    className="w-full h-20 flex justify-center items-center fixed top-0 left-0 z-50"
+    style={
+        theme === 'primary'
+            ? {
+                  backgroundImage: 'url(/assets/nav.png)', // Replace with your image path
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                  color: 'white',
+              }
+            : theme === 'secondary'
+            ? {
+                  backgroundColor: '#ffffff', 
+                  color: 'black',
+              }
+            : { backgroundColor: '', color: 'white' }
+    }
+>
+
+            
                 <div className="w-5/6 h-full flex justify-between items-center">
                     <div className="h-full flex items-center">
                         <Link to={'/'}>
                             <img
                                 src={
-                                    theme === 'primary'
+                                    theme === 'primary' || theme === 'tertiary'
                                         ? '/assets/logo/logo_white.svg'
                                         : '/assets/logo/logo_black.svg'
                                 }
